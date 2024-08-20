97d5a00c9408e8d2540800552bcbf09896dcef24
muhammadazam52564 <muhammadazam52564@gmail.com>
2024-08-20 22:35:30 +0500

    aaaa

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -537,7 +537,7 @@
                 $seven_days_date = Carbon::now()->addDays(7)->format('Y-m-d');
                 print("seven_days_date");
                 print($seven_days_date);
-                $user_ids_for_seven_days = User::pluck('id')->get();
+                $user_ids_for_seven_days = User::get();
                 
                 // $user_ids_for_seven_days = array_unique(Period::whereDate('period_start', $seven_days_date)->pluck("user_id")->toArray());
                 print("user_ids_for_seven_days");
